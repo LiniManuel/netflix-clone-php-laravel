a05c5e9d81ac739b83ae90d93607d0d443dd0d24
Manuel Lini <manuel.lini@smeup.com>
2021-11-23 12:57:05 +0100

    Add create_actors_table and create_actor_movie_table with migrations

---
+++ database/migrations/2021_11_23_115342_create_actor_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Actor;
+use App\Models\Movie;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActorMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actor_movies', function (Blueprint $table) {
+            $table->id();
+            //$table->foreignId('movie_id');
+            $table->foreignIdFor(Movie::class);
+            $table->foreignIdFor(Actor::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actor_movies');
+    }
+}
